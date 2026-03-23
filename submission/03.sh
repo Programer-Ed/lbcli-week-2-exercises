52f766d38e1ce88634ebf245f6423c8c85271eeb
Zairo <edwinmoyi8@email.com>
2026-03-23 09:54:52 +0300

    save progress 1-6

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32 )
+
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
+
+echo $ADDRESS
