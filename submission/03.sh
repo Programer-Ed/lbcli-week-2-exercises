078e1aa04e1238a983029845707711fd3f970237
Zairo <edwinmoyi8@email.com>
2026-03-24 09:35:26 +0300

    03

--- submission/03.sh
+++ submission/03.sh
@@ -2,6 +2,9 @@
 # Add funds to the address.
 # Return only the Address
 
+bitcoin-cli -regtest loadwallet "btrustwallet" > /dev/null 2>&1 || \
+bitcoin-cli -regtest createwallet "btrustwallet" > /dev/null 2>&1
+
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32 )
 
 bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
