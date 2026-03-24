1074ddba176f50058a6af4393d085b6a1d69b849
Zairo <edwinmoyi8@email.com>
2026-03-24 09:31:33 +0300

    02

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,6 @@
 # Create a new Bitcoin address, for receiving change.
 
+bitcoin-cli -regtest loadwallet "btrustwallet" > /dev/null 2>&1 || \
+bitcoin-cli -regtest createwallet "btrustwallet" > /dev/null 2>&1
+
 bitcoin-cli -regtest getrawchangeaddress 
